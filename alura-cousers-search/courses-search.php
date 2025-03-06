b3506990a5cbb1a18d8a21df8e732e57171a7f93
Murillou <murillovinicius13@hotmail.com>
2025-03-06 17:34:02 -0300

    feat: Utilizando o composer

---
+++ alura-cousers-search/courses-search.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Searcher.php';
+
+use Alura\CoursesSearch\Searcher;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$searcher = new Searcher($client, $crawler);
+$courses = $searcher->search('/cursos-online-programacao/php');
+
+foreach($courses as $course)
+{
+  echo $course . PHP_EOL;
+}
---
+++ alura-cousers-search/src/Searcher.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Alura\CoursesSearch;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Searcher
+{
+    private $httpClient;
+    private $crawler;
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+      $this->httpClient = $httpClient;
+      $this->crawler = $crawler;
+    }
+
+
+    public function search(string $url): array
+    {
+        $response = $this->httpClient->request("GET", $url);
+
+        $html = $response->getBody();
+
+        $this->crawler->addHtmlContent($html, 'UTF-8');
+
+        $coursesElements = $this->crawler->filter('span.card-curso__nome');
+        $courses = [];
+
+        foreach ($coursesElements as $courseElement) {
+          $courses[] = $courseElement->textContent;
+
+    };
+
+      return $courses;
+}
+}
