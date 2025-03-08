7033c6f95a9aca1634f82a6d85c475c11d06fd6c
Murillou <murillovinicius13@hotmail.com>
2025-03-08 16:24:49 -0300

    feat: Utilizando fetch para obter dados do usuário no banco de dados

--- screen-match/pdo/conection.php
+++ screen-match/pdo/conection.php
@@ -1,6 +1,8 @@
 <?php
 
-$srcBanco = __DIR__ . "/banco.sqlite";
-$pdo = new PDO('sqlite:' . $srcBanco);
+$databasePath = __DIR__ . "/banco.sqlite";
+$pdo = new PDO('sqlite:' . $databasePath);
 
 echo "Successful!";
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
